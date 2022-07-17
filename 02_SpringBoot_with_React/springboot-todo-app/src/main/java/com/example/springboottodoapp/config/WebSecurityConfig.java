e1465aa4e7a3cd8322c24c83999fbfe90e837c95
siwon-park <zow777@naver.com>
2022-07-18 08:16:03 +0900

    220717 SW: Springboot with react updated

---
+++ 02_SpringBoot_with_React/springboot-todo-app/src/main/java/com/example/springboottodoapp/config/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.springboottodoapp.config;
+
+import com.example.springboottodoapp.security.JwtAuthenticationFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Slf4j
+@Configuration
+public class WebSecurityConfig {
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                .csrf()
+                .disable()
+                .httpBasic()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+        http.addFilterAfter(jwtAuthenticationFilter, CorsFilter.class);
+
+        return http.build();
+    }
+
+}
---
+++ 02_SpringBoot_with_React/springboot-todo-app/src/main/java/com/example/springboottodoapp/security/JwtAuthenticationFilter.java
@@ -0,0 +1,69 @@
+package com.example.springboottodoapp.security;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter { // JWT인증 필터
+
+    private TokenProvider tokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            // 요청에서 토큰 가져오기
+            String token = parseBearerToken(request);
+            log.info("Filter is Running...");
+
+            // 토큰 검사하기(JWT이므로 인가 서버에 요청하지 않고도 검증 가능) // equalsIgnoreCase: 대소문자 구분없이 문자열 비교
+            if (token != null && !token.equalsIgnoreCase("null")) {
+                // userId 가져오기(위조된 경우 예외처리 됨)
+                String userId = tokenProvider.validateAndGetUserId(token);
+                log.info("Authenticated user ID: " + userId);
+
+                // 인증 완료. SecurityContextHolder에 등록해야 인증된 사용자
+                AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userId, // 인증된 사용자의 정보. 문자열이 아니어도 됨. 보통은 UserDetails라는 오브젝를 넣음
+                        null,
+                        AuthorityUtils.NO_AUTHORITIES
+                );
+
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+                securityContext.setAuthentication(authenticationToken);
+                SecurityContextHolder.setContext(securityContext);
+            }
+        } catch (Exception e) {
+            logger.error("Could Not Set User Authentication in Security Context", e);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String parseBearerToken(HttpServletRequest request) { // 토큰의 앞 부분인 Bearer를 제거하는 메서드
+        String bearerToken = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
